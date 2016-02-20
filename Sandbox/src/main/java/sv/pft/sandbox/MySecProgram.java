f7301c3579be924719195f5fbc134e1be50a7445
ltana <ltanaice@gmail.com>
2016-02-20 23:56:12 +0200

    Git deleted from Version control settings for project

--- Sandbox/src/main/java/sv/pft/sandbox/MySecProgram.java
+++ Sandbox/src/main/java/sv/pft/sandbox/MySecProgram.java
@@ -1,7 +1,7 @@
 package sv.pft.sandbox;
 
 public class MySecProgram {
-public static void main(String[] args){
-System.out.println("Hel world");
-}
+    public static void main(String[] args) {
+        System.out.println("Hel world");
+    }
 }
