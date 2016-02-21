8f50cd12a231e4d5431118aaa96f1c6922dba493
ltana <ltanaice@gmail.com>
2016-02-21 13:32:53 +0200

    add some methods

--- Sandbox/src/main/java/sv/pft/sandbox/MySecProgram.java
+++ Sandbox/src/main/java/sv/pft/sandbox/MySecProgram.java
@@ -2,6 +2,27 @@ package sv.pft.sandbox;
 
 public class MySecProgram {
     public static void main(String[] args) {
-        System.out.println("Hel world");
+        hello("world");
+        hello("user");
+        hello("Sv");
+
+        double l = 5;
+        System.out.println("пл кв сл стор " + l + "им пл = " + area(l));
+
+        double a = 3;
+        double b = 4;
+        System.out.println("пл пр со стор " + a + " and " + b + " is = " + area(a, b));
+    }
+
+    public static void hello(String som) {
+        System.out.println("Hel " + som + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
