2f34b8c7fc33d9a146ee845d7161c792544f8495
ltana <ltanaice@gmail.com>
2016-03-29 23:00:33 +0300

    added jCommander parameters for data generators for groups/contacts

--- addressbook-web-tests/src/test/java/sv/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/sv/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package sv.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import sv.pft.addressbook.Model.GroupData;
 
 import java.io.File;
@@ -13,15 +16,27 @@ import java.util.List;
  * Created by Ltana on 29.03.2016.
  */
 public class GroupDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        List<GroupData> groups = generateGroups(count);
-        save(groups, file);
+    @Parameter(names = "-c", description = "Groups count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
@@ -31,7 +46,12 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i=0; i<count; i++) {
             groups.add(new GroupData().withName(String.format("test %s", i))
