9c48dbfc7c90a30e5a09d4cadd52e061aab815f0
ltana <ltanaice@gmail.com>
2016-03-29 23:57:01 +0300

    added dataprovider using csv file for create contact/group

--- addressbook-web-tests/src/test/java/sv/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/sv/pft/addressbook/tests/ContactCreationTests.java
@@ -1,18 +1,54 @@
 package sv.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sv.pft.addressbook.Model.ContactData;
 import sv.pft.addressbook.Model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ContactCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line !=null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withName(split[0])
+                    .withLastname(split[1])
+                    .withAddress(split[2])
+                    .withMobilePhone(split[3])
+                    .withGroup(split[4])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreationWithoutPhoto(ContactData contact) {
+        Contacts before = app.contact().all();
+        app.contact().create(contact);
+        app.goTo().homePage();
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
+
+        assertThat(after, equalTo(before.withAdded(
+                contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
+
     @Test
-    public void testContactCreation() {
+    public void testContactCreationWithPhoto() {
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/mytests.png");
         ContactData contact = new ContactData().withName("CrName")
